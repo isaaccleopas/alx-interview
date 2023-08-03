4ff34e304e81bbc5ee5a153dacc48d2c89a00348
isaaccleopas <isaaccleopas@gmail.com>
2023-08-03 21:45:25 +0000

    locboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,9 @@
-
 #!/usr/bin/python3
 """Determines if all the boxes can be opened."""
 
 
 def canUnlockAll(boxes):
-    """Returns True if all boxes be visited, otherwise False."""
+    """Returns True if all boxes can be visited, otherwise False."""
     n = len(boxes)
     visited = [False] * n
     stack = [0]
