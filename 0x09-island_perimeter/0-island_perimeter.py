ee6f3a73aa9d5d0fbb262da1bd9bb61f5796aa0f
isaaccleopas <isaaccleopas@gmail.com>
2023-09-26 09:58:21 +0000

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Island perimeter computing module.
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of an island
+    with no lakes
+    """
+    if not grid:
+        return 0
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+    return perimeter
